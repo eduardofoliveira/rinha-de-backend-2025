f3d0cf8daa6d7050f717456dd1f22d658e9a701c
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-07-09 12:35:44 -0300

    Add Purge Payments

--- src/controller/paymentController.ts
+++ src/controller/paymentController.ts
@@ -2,18 +2,39 @@ import type { Request, Response } from 'express'
 
 import api, { api_fallback } from '../service/api'
 
+let totalRequests = 0
+let totalAmount = 0
+let totalFee = 0
+let feePerTransaction = 0.01
+
+const summary = async (req: Request, res: Response): Promise<any> => {
+  const { from, to } = req.query
+
+  return res.status(200).json({
+    totalRequests,
+    totalAmount,
+    totalFee,
+    feePerTransaction
+  })
+}
+
 const create = async (req: Request, res: Response): Promise<any> => {
-  console.log(req.body)
+  const { correlationId, amount } = req.body
 
   const { data } = await api.post('/payments', req.body)
+
+  console.log(`Payment created with correlationId: ${correlationId}, amount: ${amount}`)
+  console.log({
+    data
+  })
+
+  totalRequests++
+  totalAmount += amount
+
   return res.status(201).json(data)
 }
 
 const purge = async (req: Request, res: Response): Promise<any> => {
-  console.log(req.body)
-  console.log(req.headers)
-  console.log(req.query)
-
   const result = await Promise.all([
     api.post('/admin/purge-payments', req.body, {
       headers: {
