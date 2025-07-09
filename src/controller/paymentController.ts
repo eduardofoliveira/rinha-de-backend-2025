783d76edc4122d8f18d3bf081c862e3e0c1b816e
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-07-09 12:12:31 -0300

    Add Purge Payments

--- src/controller/paymentController.ts
+++ src/controller/paymentController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express'
 
-import api from '../service/api'
+import api, { api_fallback } from '../service/api'
 
 const create = async (req: Request, res: Response): Promise<any> => {
   console.log(req.body)
@@ -9,6 +9,16 @@ const create = async (req: Request, res: Response): Promise<any> => {
   return res.status(201).json(data)
 }
 
+const purge = async (req: Request, res: Response): Promise<any> => {
+  const result = await Promise.all([
+    api.post('/purge-payments', req.body),
+    api_fallback.post('/purge-payments', req.body),
+  ])
+
+  return res.status(200).json(result)
+}
+
 export default {
-  create
+  create,
+  purge
 }
--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -5,5 +5,6 @@ import paymentController from '../controller/paymentController'
 const router = Router()
 
 router.post('/payments', paymentController.create)
+router.post('/purge-payments', paymentController.purge)
 
 export default router
