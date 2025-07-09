ae6b5053f3df372c244d6f2373b27ff8374e1adc
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-07-09 12:43:19 -0300

    Add Purge Payments

--- src/controller/paymentController.ts
+++ src/controller/paymentController.ts
@@ -3,7 +3,7 @@ import type { Request, Response } from 'express'
 import api, { api_fallback } from '../service/api'
 
 let totalRequests = 0
-let totalAmount = 0
+let totalAmountCents = 0
 let totalFee = 0
 let feePerTransaction = 0.01
 
@@ -12,7 +12,7 @@ const summary = async (req: Request, res: Response): Promise<any> => {
 
   return res.status(200).json({
     totalRequests,
-    totalAmount,
+    totalAmount: (totalAmountCents / 100).toFixed(2),
     totalFee,
     feePerTransaction
   })
@@ -29,7 +29,7 @@ const create = async (req: Request, res: Response): Promise<any> => {
   })
 
   totalRequests++
-  totalAmount += amount
+  totalAmountCents += Math.round(Number(amount) * 100)
 
   return res.status(201).json(data)
 }
