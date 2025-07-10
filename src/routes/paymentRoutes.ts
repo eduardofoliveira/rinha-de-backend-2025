c99d0d3dac54a3e26b1bc8a90e8cd9bef4bfa17f
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-07-10 07:21:29 -0300

    Add Nginx Proxy Reverse

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -7,5 +7,8 @@ const router = Router()
 router.get('/payments-summary', paymentController.summary)
 router.post('/payments', paymentController.create)
 router.post('/purge-payments', paymentController.purge)
+router.get('/env', (req: Request, res: Response): Response => {
+  return res.status(200).json(process.env)
+})
 
 export default router
